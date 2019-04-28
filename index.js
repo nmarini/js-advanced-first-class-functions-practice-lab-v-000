fb6ffb3e95e3d2fa1ebec7f7cddcd37dd71289d0
Nicolas Marini <nmarini44@gmail.com>
2019-04-28 21:01:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function logDriverNames(array){
     console.log(driver.name)
   })
 }
+
+function driversByRevenue(array, location){
+  return array.forEach(function(driver){
+    if (driver.location == location) {
+      console.log(driver.name)
+    }
+  })
+}
